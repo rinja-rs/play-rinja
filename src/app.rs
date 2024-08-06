10b3ec0399930b239a771f3ce4594e91b6b0d2a1
René Kijewski <rene.kijewski@fu-berlin.de>
2024-08-06 03:30:24 +0200

    Add syntax highlighting

---
+++ src/app.rs
@@ -0,0 +1,164 @@
+use std::rc::Rc;
+use std::time::Duration;
+
+use prettyplease::unparse;
+use proc_macro2::TokenStream;
+use rinja_derive_standalone::derive_template;
+use syn::{parse2, parse_quote};
+use web_sys::wasm_bindgen::prelude::Closure;
+use web_sys::wasm_bindgen::{JsCast, UnwrapThrowExt};
+use web_sys::window;
+use yew::{
+    function_component, html, use_state, Callback, Html, Properties, SubmitEvent, UseStateHandle,
+};
+
+use crate::editor::Editor;
+
+#[derive(Properties, PartialEq, Clone)]
+pub struct Props {
+    rust: Rc<str>,
+    tmpl: Rc<str>,
+    code: Rc<str>,
+    duration: Option<Duration>,
+    timeout: Option<i32>,
+}
+
+#[function_component]
+pub fn App() -> Html {
+    let state = use_state(|| {
+        let (code, duration) = convert_source(STRUCT_SOURCE, TMPL_SOURCE);
+        Props {
+            rust: Rc::from(STRUCT_SOURCE),
+            tmpl: Rc::from(TMPL_SOURCE),
+            code: Rc::from(code),
+            duration,
+            timeout: None,
+        }
+    });
+
+    let duration = state.duration.map(|d| format!(" (duration: {d:?})"));
+
+    let onsubmit = Callback::from(|ev: SubmitEvent| {
+        ev.prevent_default();
+        ev.stop_propagation();
+    });
+
+    let oninput = |edit: fn(&mut Props, String)| {
+        let state = state.clone();
+        move |data: String| {
+            let mut new_state = Props::clone(&*state);
+            edit(&mut new_state, data);
+            replace_timeout(&mut new_state, state.clone());
+            state.set(new_state);
+        }
+    };
+    let oninput_rust = oninput(|new_state, data| new_state.rust = Rc::from(data));
+    let oninput_tmpl = oninput(|new_state, data| new_state.tmpl = Rc::from(data));
+
+    html! {
+        <form method="GET" action="javascript:;" {onsubmit}>
+            <div class="top">
+                <div>
+                    <h3> {"Your struct:"} </h3>
+                    <Editor
+                        text={Rc::clone(&state.rust)}
+                        oninput={oninput_rust}
+                        syntax="Rust"
+                    />
+                </div>
+                <div>
+                    <h3> {"Your template:"} </h3>
+                    <Editor
+                        text={Rc::clone(&state.tmpl)}
+                        oninput={oninput_tmpl}
+                        syntax="HTML"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <h3> {"Generated code:"} {duration} </h3>
+                <Editor
+                    text={Rc::clone(&state.code)}
+                    syntax="Rust"
+                />
+            </div>
+        </form>
+    }
+}
+
+fn replace_timeout(new_state: &mut Props, state: UseStateHandle<Props>) {
+    let handler = Closure::<dyn Fn()>::new({
+        let rust = Rc::clone(&new_state.rust);
+        let tmpl = Rc::clone(&new_state.tmpl);
+        let state = state.clone();
+        move || {
+            let (code, duration) = convert_source(&rust, &tmpl);
+            state.set(Props {
+                rust: Rc::clone(&rust),
+                tmpl: Rc::clone(&tmpl),
+                code: Rc::from(code),
+                duration,
+                timeout: None,
+            });
+        }
+    });
+
+    let window = window().unwrap_throw();
+    if let Some(timeout) = new_state.timeout {
+        window.clear_timeout_with_handle(timeout);
+    }
+    new_state.timeout = window
+        .set_timeout_with_callback_and_timeout_and_arguments_0(
+            handler.into_js_value().unchecked_ref(),
+            500,
+        )
+        .ok();
+}
+
+fn convert_source(rust: &str, tmpl: &str) -> (String, Option<Duration>) {
+    let mut code: TokenStream = parse_quote! { #[template(ext = "html", source = #tmpl)] };
+    code.extend(rust.parse::<TokenStream>());
+    let (code, duration) = time_it(|| derive_template(code));
+    let code = unparse(&parse2(code).unwrap_throw());
+    (code, duration)
+}
+
+fn time_it<F: FnOnce() -> R, R>(func: F) -> (R, Option<Duration>) {
+    let performance = window().unwrap_throw().performance();
+    let start = performance.as_ref().map(|p| p.now());
+    let result = func();
+    let end = performance.as_ref().map(|p| p.now());
+    let duration = match (start, end) {
+        (Some(start), Some(end)) => Duration::try_from_secs_f64((end - start) / 1000.0).ok(),
+        _ => None,
+    };
+    (result, duration)
+}
+
+const TMPL_SOURCE: &str = r##"<div class="example-wrap"> {# #}
+    <div data-nosnippet><pre class="src-line-numbers">
+        {% for line in lines.clone() %}
+            {% if embedded %}
+                <span>{{line|safe}}</span>
+            {%~ else %}
+                <a href="#{{line|safe}}" id="{{line|safe}}">{{line|safe}}</a>
+            {%~ endif %}
+        {% endfor %}
+    </pre></div> {# #}
+    <pre class="rust"> {# #}
+        <code>
+            {% if needs_expansion %}
+                <button class="expand">&varr;</button>
+            {% endif %}
+            {{code_html|safe}}
+        </code> {# #}
+    </pre> {# #}
+</div>"##;
+
+const STRUCT_SOURCE: &str = r##"struct Source<Code: std::fmt::Display> {
+    embedded: bool,
+    needs_expansion: bool,
+    lines: RangeInclusive<usize>,
+    code_html: Code,
+}"##;
---
+++ src/editor.rs
@@ -0,0 +1,141 @@
+use std::fmt::Write;
+use std::rc::Rc;
+
+use once_cell::sync::Lazy;
+use syntect::easy::HighlightLines;
+use syntect::highlighting::{Color, FontStyle, ThemeSet};
+use syntect::parsing::SyntaxSet;
+use syntect::util::LinesWithEndings;
+use web_sys::wasm_bindgen::{JsCast, UnwrapThrowExt};
+use web_sys::HtmlTextAreaElement;
+use yew::{function_component, html, Callback, Html, InputEvent, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct EditorProps {
+    pub text: Rc<str>,
+    #[prop_or_default]
+    pub oninput: Callback<String>,
+    pub syntax: &'static str,
+}
+
+#[function_component]
+pub fn Editor(props: &EditorProps) -> Html {
+    let EditorProps {
+        text,
+        oninput,
+        syntax,
+    } = props;
+    html! {
+        <div class="editor">
+            <UnstylizedCode text={Rc::clone(text)} {oninput} />
+            <StylizedCode text={Rc::clone(text)} syntax={*syntax} />
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+pub struct UnstylizedCodeProps {
+    pub text: Rc<str>,
+    #[prop_or_default]
+    pub oninput: Option<Callback<String>>,
+}
+
+#[function_component]
+pub fn UnstylizedCode(props: &UnstylizedCodeProps) -> Html {
+    let oninput = props.oninput.clone().map(|oninput| {
+        Callback::from({
+            let old_text = Rc::clone(&props.text);
+            move |ev: InputEvent| {
+                let Some(target) = ev.target() else { return };
+                let target: HtmlTextAreaElement = target.unchecked_into();
+                let new_text = target.value();
+                if new_text != *old_text {
+                    oninput.emit(new_text);
+                }
+            }
+        })
+    });
+
+    html! {
+        <textarea
+            autocapitalize="off"
+            spellcheck="false"
+            readonly={oninput.is_none()}
+            value={Rc::clone(&props.text)}
+            {oninput}
+        />
+    }
+}
+
+#[function_component]
+pub fn StylizedCode(props: &StylizedCodeProps) -> Html {
+    let syntax = SYNTAX_SET.find_syntax_by_name(props.syntax).unwrap_throw();
+    let theme = &THEME_SET.themes["InspiredGitHub"];
+
+    let fg = theme.settings.foreground.unwrap_or(Color::BLACK);
+    let bg = theme.settings.background.unwrap_or(Color::WHITE);
+    let mut highlighter = HighlightLines::new(syntax, theme);
+    let mut output = Vec::new();
+    let mut rc_style: Option<Rc<str>> = None;
+
+    for line in LinesWithEndings::from(&props.text) {
+        let regions = highlighter.highlight_line(line, &SYNTAX_SET).unwrap_throw();
+        for (s, text) in regions {
+            let mut style = String::new();
+
+            if s.foreground != fg {
+                style.push_str("color:");
+                write_css_color(&mut style, s.foreground);
+                style.push(';');
+            }
+            if s.background != bg {
+                style.push_str("background-color:");
+                write_css_color(&mut style, s.background);
+                style.push(';');
+            }
+            if s.font_style.contains(FontStyle::BOLD) {
+                style.push_str("font-weight:bold;");
+            }
+            if s.font_style.contains(FontStyle::UNDERLINE) {
+                style.push_str("text-decoration:underline;");
+            }
+            if s.font_style.contains(FontStyle::ITALIC) {
+                style.push_str("font-style:italic;");
+            }
+
+            let style = match style.is_empty() {
+                true => None,
+                false => Some(style),
+            };
+            if style.as_deref() != rc_style.as_deref() {
+                rc_style = style.map(Rc::from);
+            }
+
+            output.push(html!(<span style={rc_style.clone()}>{text}</span>));
+        }
+    }
+
+    let style = format!(
+        "color:#{:02x}{:02x}{:02x};background-color:#{:02x}{:02x}{:02x};",
+        fg.r, fg.g, fg.b, bg.r, bg.g, bg.b,
+    );
+
+    html!(<pre {style}> {output} {"\u{feff}"} </pre>)
+}
+
+#[derive(Properties, PartialEq)]
+pub struct StylizedCodeProps {
+    pub text: Rc<str>,
+    pub syntax: &'static str,
+}
+
+fn write_css_color(s: &mut String, c: Color) {
+    if c.a != 0xFF {
+        write!(s, "#{:02x}{:02x}{:02x}{:02x}", c.r, c.g, c.b, c.a).unwrap_throw();
+    } else {
+        write!(s, "#{:02x}{:02x}{:02x}", c.r, c.g, c.b).unwrap_throw();
+    }
+}
+
+static SYNTAX_SET: Lazy<SyntaxSet> = Lazy::new(SyntaxSet::load_defaults_newlines);
+static THEME_SET: Lazy<ThemeSet> = Lazy::new(ThemeSet::load_defaults);
